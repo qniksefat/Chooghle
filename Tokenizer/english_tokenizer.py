48ce28565afcfc59d7964bcd1860481c194d1d3d
qniksefat <qniksefat@gmail.com>
2019-11-29 17:56:04 +0330

    stemmed data

--- Tokenizer/english_tokenizer.py
+++ Tokenizer/english_tokenizer.py
@@ -1,13 +1,18 @@
 from __future__ import unicode_literals
 import nltk
 # nltk.download('punkt')
-nltk.download('wordnet')
+# nltk.download('wordnet')
 # if there was any problem with NLTK, uncomment the line above
-from nltk import word_tokenize, sent_tokenize
+from nltk import word_tokenize
+from nltk.stem.lancaster import LancasterStemmer
 import os
 import csv
 from nltk.stem import WordNetLemmatizer
 
+from Tokenizer import foo_func
+foo_func.foo()
+
+st = LancasterStemmer()
 wnl = WordNetLemmatizer()
 
 
@@ -39,10 +44,13 @@ def english_tokenize(data):
     for datum in data:
         word_tokenized = word_tokenize((datum['content']))
         lemmatized = [wnl.lemmatize(word) for word in word_tokenized]
-        datum['content'] = lemmatized
+        stemmed = [st.stem(word) for word in lemmatized]
+        datum['content'] = stemmed
         word_tokenized = word_tokenize((datum['title']))
         lemmatized = [wnl.lemmatize(word) for word in word_tokenized]
-        datum['title'] = lemmatized
+        stemmed = [st.stem(word) for word in lemmatized]
+        datum['title'] = stemmed
+    return data
 
 
 def add_doc():
@@ -51,3 +59,7 @@ def add_doc():
 
 data = read_from_file()
 data = english_tokenize(data)
+print(
+    data[0], '\n',
+    data[1]
+)
